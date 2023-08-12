d564dd86c2a65d74a56397df30f03bcdb2a144f9
akinabudu <orglobalng@gmail.com>
2023-08-12 10:16:11 +0000

    Update:portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Akinbola Laide-Abudu
+        I'm Akinbola Laide-Abudu, welcome to my space.
          
         </h2>
       </div>
