fafa910b95e102aa0c6e399893cb377182f7bc41
akinabudu <orglobalng@gmail.com>
2023-08-20 21:51:00 +0000

    Add:Pitch

--- app/pitch/page.tsx
+++ app/pitch/page.tsx
@@ -6,9 +6,9 @@ export default function Page() {
   return (
     <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
-      <div className="container flex flex-col items-center justify-center min-h-screen w-1/2 px-4 mx-auto">
+      <div className="container flex flex-col items-center justify-center min-h-screen w-1/2 px-4  mx-auto">
       <div className="max-w-2xl mx-auto lg:mx-0">
-					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 my-10 sm:text-4xl">
 						Elevator Pitch
 					</h2>
 					{/* <p className="mt-4 text-zinc-400">
@@ -16,14 +16,12 @@ export default function Page() {
 					</p> */}
 				</div>
         <Card>
-          <Link
-            href={"s.href"}
-            target="_blank"
-            className="text-white/50 text-xl font-thin italic p-4 relative flex flex-col items-end  justify-center  duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-10"
-          >
-            {" "}
-            Elevator Pitch Here
-          </Link>
+         
+            <iframe
+            width="full"
+                height="350"
+                src={"https://youtube.com/embed/5vLrR-T3jAw"}>
+            </iframe>
         </Card>
       </div>
     </div>
